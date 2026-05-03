db20e31843dece649251f02687e3cc827707f953
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-05-03 14:56:56 +0400

    feat: implement FilmService with Signals and mock data

---
+++ src/app/core/services/film.service.ts
@@ -0,0 +1,24 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Film } from '../models/film.model';
+import filmsData from '../../../assets/data/films.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmService {
+  private readonly filmsSignal = signal<Film[]>(filmsData);
+
+  readonly films = this.filmsSignal.asReadonly();
+
+  readonly favoriteFilms = computed(() => this.filmsSignal().filter((film) => film.isFavorite));
+
+  getFilmById(id: number): Film | undefined {
+    return this.filmsSignal().find((f) => f.id === id);
+  }
+
+  toggleFavorite(id: number): void {
+    this.filmsSignal.update((films) =>
+      films.map((f) => (f.id === id ? { ...f, isFavorite: !f.isFavorite } : f)),
+    );
+  }
+}
